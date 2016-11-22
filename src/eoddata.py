5a8bc12533f55d8a824ba48f1a838d753fcd6c24
Adrian Torrie <adriantorrie@gmail.com>
2016-11-23 01:39:13 +1100

    added src dir with eoddata

---
+++ src/eoddata.py
@@ -0,0 +1,7 @@
+"""Common methods and constants for eoddata functions"""
+
+# webservice url
+WS_URL = 'http://ws.eoddata.com/data.asmx'
+
+# returning xml responses prefixed with this namespace
+XMLNS = 'http://ws.eoddata.com/Data'
